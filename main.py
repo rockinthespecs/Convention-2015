76bf9dbc56885c9797abbd3b4adc9de97db86802
rockinthespecs <kaitlyn.sandor@gmail.com>
2016-06-19 14:08:03 -0400

    Updated countfield
    changed range.field to count.field -> if a person shades 5 boxes for 5 instead of just one, will pick up 5
    
    count.field -> highest shaded number
    range.field -> darkest shaded number

--- main.py
+++ main.py
@@ -1,93 +1,93 @@
-from presenter import * #What does this do?
+from panel import * #What does this do?
 
 # Main method to process a full-page sheet
 # Submits three times, because there are three matches on one sheet
-def main(presenter):
+def main(panel):
 	for s in (0,16,32): #What the heck does this mean?
-	  presenter.shiftDown(s)
+	  panel.shiftDown(s)
 
 		# The numberings of the fields are important; you reference them by number in server.py
-		bkks = presenter.rangefield('N-11', 0, 4) #Barbara Katz Knowledge Subject
-		presenter.set("BK Knowledge of Subject", bkks) #0
+		bkks = panel.countfield('N-11', 0, 4) #Barbara Katz Knowledge Subject
+		panel.set("BK Knowledge of Subject", bkks) #0
 		
-		bkop = presenter.rangefield('N-12', 0, 4) #Barbara Katz Organized in Presenting
-		presenter.set("BK Organized in Presenting", bkop) #1
+		bkop = panel.countfield('N-12', 0, 4) #Barbara Katz Organized in Presenting
+		panel.set("BK Organized in Presenting", bkop) #1
 		
-		bkcd = presenter.rangefield('N-13', 0, 4) #Barbara Katz Effective in Content Delivery
-		presenter.set("BK Effective in Content Delivery", bkcd) #2
+		bkcd = panel.countfield('N-13', 0, 4) #Barbara Katz Effective in Content Delivery
+		panel.set("BK Effective in Content Delivery", bkcd) #2
 		
-		bktm = presenter.rangefield('N-14', 0, 4) #Barbara Katz Teaching Methods Used Effectively
-		presenter.set("BK Teaching Methods Used Effectively", bktm) #3
+		bktm = panel.countfield('N-14', 0, 4) #Barbara Katz Teaching Methods Used Effectively
+		panel.set("BK Teaching Methods Used Effectively", bktm) #3
 		
-		pmks = presenter.rangefield('AD-11', 0, 4) #Pamela D. Miller Knowledge Subject
-		presenter.set("PM Knowledge of Subject", pmks) #4
+		pmks = panel.countfield('AD-11', 0, 4) #Pamela D. Miller Knowledge Subject
+		panel.set("PM Knowledge of Subject", pmks) #4
 		
-		pmop = presenter.rangefield('AD-12', 0, 4) #Pamela D. Miller Organized in Presenting
-		presenter.set("PM Organized in Presenting", pmop) #5
+		pmop = panel.countfield('AD-12', 0, 4) #Pamela D. Miller Organized in Presenting
+		panel.set("PM Organized in Presenting", pmop) #5
 		
-		pmcd = presenter.rangefield('AD-13', 0, 4) #Pamela D. Miller Effective in Content Delivery
-		presenter.set("PM Effective in Content Delivery", pmcd) #6
+		pmcd = panel.countfield('AD-13', 0, 4) #Pamela D. Miller Effective in Content Delivery
+		panel.set("PM Effective in Content Delivery", pmcd) #6
 		
-		pmtm = presenter.rangefield('AD-14', 0, 4) #Pamela D. Miller Teaching Methods Used Effectively
-		presenter.set("PM Teaching Methods Used Effectively", pmtm) #7
+		pmtm = panel.countfield('AD-14', 0, 4) #Pamela D. Miller Teaching Methods Used Effectively
+		panel.set("PM Teaching Methods Used Effectively", pmtm) #7
 		
-		jmks = presenter.rangefield('N-17', 0, 4) #Julia MacMillan Knowledge Subject
-		presenter.set("JM Knowledge of Subject", jmks) #8
+		jmks = panel.countfield('N-17', 0, 4) #Julia MacMillan Knowledge Subject
+		panel.set("JM Knowledge of Subject", jmks) #8
 		
-		jmop = presenter.rangefield('N-18', 0, 4) #Julia MacMillan Organized in Presenting
-		presenter.set("JM Organized in Presenting", jmop) #9
+		jmop = panel.countfield('N-18', 0, 4) #Julia MacMillan Organized in Presenting
+		panel.set("JM Organized in Presenting", jmop) #9
 		
-		jmcd = presenter.rangefield('N-19', 0, 4) #Julia MacMillan Effective in Content Delivery
-		presenter.set("JM Effective in Content Delivery", jmcd) #10
+		jmcd = panel.countfield('N-19', 0, 4) #Julia MacMillan Effective in Content Delivery
+		panel.set("JM Effective in Content Delivery", jmcd) #10
 		
-		jmtm = presenter.rangefield('N-20', 0, 4) #Julia MacMillan Teaching Methods Used Effectively
-		presenter.set("JM Teaching Methods Used Effectively", jmtm) #11
+		jmtm = panel.countfield('N-20', 0, 4) #Julia MacMillan Teaching Methods Used Effectively
+		panel.set("JM Teaching Methods Used Effectively", jmtm) #11
 		
-		tkks = presenter.rangefield('AD-17', 0, 4) #Tara Kelly Knowledge Subject
-		presenter.set("TK Knowledge of Subject", tkks) #12
+		tkks = panel.countfield('AD-17', 0, 4) #Tara Kelly Knowledge Subject
+		panel.set("TK Knowledge of Subject", tkks) #12
 		
-		tkop = presenter.rangefield('AD-18', 0, 4) #Tara Kelly Organized in Presenting
-		presenter.set("TK Organized in Presenting", tkop) #13
+		tkop = panel.countfield('AD-18', 0, 4) #Tara Kelly Organized in Presenting
+		panel.set("TK Organized in Presenting", tkop) #13
 		
-		tkcd = presenter.rangefield('AD-19', 0, 4) #Tara Kelly Effective in Content Delivery
-		presenter.set("TK Effective in Content Delivery", tkcd) #14
+		tkcd = panel.countfield('AD-19', 0, 4) #Tara Kelly Effective in Content Delivery
+		panel.set("TK Effective in Content Delivery", tkcd) #14
 		
-		tktm = presenter.rangefield('AD-20', 0, 4) #Tara Kelly Teaching Methods Used Effectively
-		presenter.set("TK Teaching Methods Used Effectively", tktm) #15
+		tktm = panel.countfield('AD-20', 0, 4) #Tara Kelly Teaching Methods Used Effectively
+		panel.set("TK Teaching Methods Used Effectively", tktm) #15
 		
-	  ebks = presenter.rangefield('V-23', 0, 4) #Emily Bialy Knowledge Subject
-		presenter.set("EB Knowledge of Subject", ebks) #16
+	  ebks = panel.countfield('V-23', 0, 4) #Emily Bialy Knowledge Subject
+		panel.set("EB Knowledge of Subject", ebks) #16
 		
-		ebop = presenter.rangefield('V-24', 0, 4) #Emily Bialy Organized in Presenting
-		presenter.set("EB Organized in Presenting", ebop) #17
+		ebop = panel.countfield('V-24', 0, 4) #Emily Bialy Organized in Presenting
+		panel.set("EB Organized in Presenting", ebop) #17
 		
-		ebcd = presenter.rangefield('V-25', 0, 4) #Emily Bialy Effective in Content Delivery
-		presenter.set("EB Effective in Content Delivery", ebcd) #18
+		ebcd = panel.countfield('V-25', 0, 4) #Emily Bialy Effective in Content Delivery
+		panel.set("EB Effective in Content Delivery", ebcd) #18
 		
-		ebtm = presenter.rangefield('V-26', 0, 4) #Emily Bialy Teaching Methods Used Effectively
-		presenter.set("EB Teaching Methods Used Effectively", ebtm) #19
+		ebtm = panel.countfield('V-26', 0, 4) #Emily Bialy Teaching Methods Used Effectively
+		panel.set("EB Teaching Methods Used Effectively", ebtm) #19
 		
-		self1 = presenter.rangefield('AA-34', 0, 4) #Self Eval 1
-		presenter.set("Communication", self1) #20
+		self1 = panel.countfield('AA-34', 0, 4) #Self Eval 1
+		panel.set("Communication", self1) #20
 		
-		self2 = presenter.rangefield('AA-35', 0, 4) #Self Eval 2
-		presenter.set("Ethics", self2) #21
+		self2 = panel.countfield('AA-35', 0, 4) #Self Eval 2
+		panel.set("Ethics", self2) #21
 		
-		self3 = presenter.rangefield('AA-36', 0, 4) #Self Eval 3
-		presenter.set("Advocacy", self3) #22
+		self3 = panel.countfield('AA-36', 0, 4) #Self Eval 3
+		panel.set("Advocacy", self3) #22
 		
-		self4 = presenter.rangefield('AA-37', 0, 4) #Self Eval 4
-		presenter.set("Objectives relation to purpose of educational activity", self4) #23
+		self4 = panel.countfield('AA-37', 0, 4) #Self Eval 4
+		panel.set("Objectives relation to purpose of educational activity", self4) #23
 		
-		num1 = presenter.rangefield('M-39', 0, 1)
+		num1 = panel.countfield('M-39', 0, 1)
 		if num1 == 0:
 		  bias = "No"
 		elif num1 == 1:
 		  bias = "Yes"
-		presenter.set("Bias", bias)) #24
+		panel.set("Bias", bias)) #24
 
-		presenter.submit()
+		panel.submit()
 
-# This line creates a new Pipresenter and starts the program
+# This line creates a new Pipanel and starts the program
 # It takes the main function as an argument and runs it when it finds a new sheet
-Pipresenter(main)
+Pipanel(main)
